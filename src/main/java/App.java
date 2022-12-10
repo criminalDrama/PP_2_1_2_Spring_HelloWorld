e7ff726cae7728fd6853adeb528dbdb9f82c57fe
criminalDrama <kanberov7@mail.ru>
2022-12-10 09:30:33 +0300

    it was 15 minute task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        boolean a = bean == bean2;
+        System.out.println(a);
+        Cat bean3 =
+                (Cat) applicationContext.getBean("Cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("Cat");
+        boolean b = bean3 == bean4;
+        System.out.println(b);
         System.out.println(bean.getMessage());
     }
 }
